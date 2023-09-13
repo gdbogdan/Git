a55a7bbf2d0650ed35d41b88daab438a155f8b96
gdbogdan <79273809+gdbogdan@users.noreply.github.com>
2023-09-13 11:18:24 +0200

    esgaarrr update
    esgaarrr

--- a1.c
+++ a1.c
@@ -6,5 +6,7 @@ int main(){
     printf("Hola mundo, soy Esgaarrr 3\n");
     printf("Hola mundo, soy Esgaarrr 4\n");
     printf("Hola mundo, soy Esgaarrr 5\n");
+    printf("Hola mundo, soy Esgaarrr 4332\n");
     printf("Hola mundo, soy Esgaarrr 6\n");
+
 }
