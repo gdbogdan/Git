b25a043f6f0bf13c6a70ae9c8d1e9f3b6762ad5c
gdbogdan <143821586+esgaarrr@users.noreply.github.com>
2023-09-14 14:05:51 +0200

    modificacion
    modificación

--- a1.c
+++ a1.c
@@ -8,5 +8,6 @@ int main(){
     printf("Hola mundo, soy Esgaarrr 5\n");
     printf("Hola mundo, soy Esgaarrr 4332\n");
     printf("Hola mundo, soy Esgaarrr 6\n");
+    printf("Nueva modificación\n");
 
 }
