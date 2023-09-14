da88f6750232ef52c57e0b5b78fba64648290ecb
gdbogdan <79273809+gdbogdan@users.noreply.github.com>
2023-09-14 14:02:09 +0200

    update
    update

--- a1.c
+++ a1.c
@@ -3,8 +3,10 @@
 int main(){
     printf("Hola mundo, soy Esgaarrr\n");
     printf("Hola mundo, soy Esgaarrr 2\n");
-    printf("Hola mundo, soy Esgaarrr s\n");
-    printf("Hola mundo, soy Esgaarrr 444\n");
+    printf("Hola mundo, soy Esgaarrr 3\n");
+    printf("Hola mundo, soy Esgaarrr 4\n");
     printf("Hola mundo, soy Esgaarrr 5\n");
-    printf("Hola mundo, soy Esgaarrr     \n");
+    printf("Hola mundo, soy Esgaarrr 4332\n");
+    printf("Hola mundo, soy Esgaarrr 6\n");
+
 }
